cf48b5b9cfce06212a95463e30e5d17e08b1cece
Ramsey Ndifor <ramseyndifun@gmail.com>
2025-03-17 20:53:20 -0500

    adding flask app

--- ecs-app/acm.tf
+++ ecs-app/acm.tf
@@ -13,22 +13,22 @@ resource "aws_acm_certificate" "wildcard_cert" {
   }
 }
 
-# Add DNS validation records to the hosted zone
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.wildcard_cert.domain_validation_options : dvo.domain_name => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
-  }
+# # Add DNS validation records to the hosted zone
+# resource "aws_route53_record" "cert_validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.wildcard_cert.domain_validation_options : dvo.domain_name => {
+#       name  = dvo.resource_record_name
+#       type  = dvo.resource_record_type
+#       value = dvo.resource_record_value
+#     }
+#   }
 
-  zone_id = data.aws_route53_zone.r53_hosted_zone.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.value]
-  ttl     = 60
-}
+#   zone_id = data.aws_route53_zone.r53_hosted_zone.zone_id
+#   name    = each.value.name
+#   type    = each.value.type
+#   records = [each.value.value]
+#   ttl     = 60
+# }
 
 # # Validate the certificate
 # resource "aws_acm_certificate_validation" "wildcard_cert_validation" {
